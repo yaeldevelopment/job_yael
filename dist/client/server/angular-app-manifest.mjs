3823e4f0e529622e6e708de6508ca56ff1dbcc4a
yaelajami2 <y0556722091@gmail.com>
2025-03-23 22:57:59 +0200

    widget

--- dist/client/server/angular-app-manifest.mjs
+++ dist/client/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 5317, hash: '2bea4a8d2568dde87df032db31badf13a32dca6c6647f1eb9e6748f95d77d92e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1010, hash: '805661c862be8ee9eb05f9f7a395be3b5b63b4a10b6761430ec4df1d2921001e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5403, hash: '01a699bbf4f82872ed407a7ba2d2eb257658ccd6727eb8e20f2a3501042f8a1d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1096, hash: '61bddb21c2d98022000b459e3458997e40265e91a6b83007adba89df7668ae23', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-UVLAIPNA.css': {size: 228103, hash: '/nOT3KqItKw', text: () => import('./assets-chunks/styles-UVLAIPNA_css.mjs').then(m => m.default)}
   },
 };
